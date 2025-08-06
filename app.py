e96b865c1429310253885df097d2d60935b5ec5a
Eduardo 465 <hiasd12e@gmail.com>
2025-08-06 04:40:42 -0600

    first commit

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 import logging
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 logging.basicConfig(level=logging.DEBUG)
 
 # Cargar modelos entrenados
